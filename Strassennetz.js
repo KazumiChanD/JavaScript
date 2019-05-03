343570903c5c3738117cba47931c2910594f8498
KazumiChanD <l.reimann@zooroyal.de>
2019-05-03 16:52:32 +0200

    Kommentare in die Globale Variante angepasst UML

--- Strassennetz.js
+++ Strassennetz.js
@@ -1,20 +1,19 @@
-//Umlaute ändern! x
-//Schleife auf variable Länge zuschneiden x
-//Function auslagern x
-//Räder in eine Variable auslagern x
-//if zusammenfassen x
-//Functionen aufteilen und eine Main Function erstellen x
-
 let Citys = ["Crivitz", "Schwerin", "Parchim", "Rostock", "Köln"];
 let car = { color: "yellow", };
 let tire = { strain: "2,5 bar", };
-let street = { success: true, };
 let raeder = 4;
 
-//die Main Methode initialisiert die new Operator
+/**
+ * Initialisiert die 2 new Operator Auto und Street
+ */
+
 function Main() {
 
-    //Damit werden Anzahl an Citys.length Autos erstellt
+    /**
+     * Anzahl an Citys.length Autos werden erstellt
+     * @param {array} Citys wird benötigt
+     * @param {string} raeder wird benötigt
+     */
     for (let i = 0; i < Citys.length ; i++) {
         Auto(4, Citys[i]);
     }
@@ -24,7 +23,13 @@ function Main() {
     console.log(myStreets.succesfull());
 }
 
-//überprüft ob der 1 Parameter und der 2 Parameter existieren
+/**
+ * Überprüft ob Stadt1 und Stadt2 existieren
+ *
+ * @param {string} Stadt1 wird benötigt
+ * @param {string} Stadt2 wird benötigt
+ *
+ */
 function Street(Stadt1, Stadt2) {
     this.Stadt1 = Stadt1;
     this.Stadt2 = Stadt2;
@@ -36,6 +41,11 @@ function Street(Stadt1, Stadt2) {
             console.log(Stadt2 + " existiert auch, also gibt es diese Straße.");
         }
     }
+    /**
+     *
+     * @return {string} succesfullStreet enthält Stadt1 und Stadt2
+     *
+     */
     this.succesfull = function() {
         let succesfullStreet = Symbol();
         succesfullStreet = Stadt1 + " " + Stadt2;
@@ -43,7 +53,13 @@ function Street(Stadt1, Stadt2) {
     }
 }
 
-//überprüft ob das Auto 4 Räder und einen Aufenthaltsort besitzt
+/**
+ * Überprüft ob das Auto 4 Räder hat und einen Aufenthaltsort besitzt
+ *
+ * @param {string} raeder wird benötigt
+ * @param {string} aufenthaltsort wird benötigt
+ *
+ */
 function Auto(raeder, aufenthaltsort) {
     this.raeder = raeder;
     this.aufenthaltsort = aufenthaltsort;
@@ -51,6 +67,14 @@ function Auto(raeder, aufenthaltsort) {
         console.log("Jetzt darf das Auto von " + aufenthaltsort + " los fahren.");
     } else {
         console.log("Hält sich zur Zeit das Auto in einer Stadt auf?");
+    }
+    /**
+     *
+     * Soll das Auto bewegen
+     *
+     */
+    let move = function() {
+
     }
 }
 
